eed2d579f4e4302252f6516cfc15387215639f7f
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-06 13:40:39 +0100

    intro2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 console.log("test - ist JS mit html richtig verknüpft ? Dann kann man das im Browser F12 bei der Konsole lesen");
+
+let varName = "SuperCoder"
+
+
+
+function intro2(paramName){
+console.log ("Hi, " + varName + ". Mein Name ist " + paramName + " .")
+
+}
+
+
+intro2("test")
+intro2(2)
